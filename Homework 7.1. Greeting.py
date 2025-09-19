3bc5b09301efff41dd686b496d1f38a74a51edf7
Kostiantyn Sobolewvkyi <k.sobolevskyi1977@gmail.com>
2025-09-19 03:04:53 +0000

    Homework 7.1. Greeting.py

--- Homework 7.1. Greeting.py
+++ Homework 7.1. Greeting.py
@@ -12,8 +12,15 @@ def say_hi(name: str, age: int) -> str:
 
 
 if __name__ == "__main__":
-    # Тести
+    # Тести (перевірка роботи функції)
     assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
     assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
     print("ОК")
+
+    # Додатково: ручне введення від користувача
+    user_name = input("Введіть ім'я: ")
+    user_age = int(input("Введіть вік: "))
+    print(say_hi(user_name, user_age))
+
+
 
